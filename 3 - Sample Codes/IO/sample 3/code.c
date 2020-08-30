98014c1993b077c88f36e83e1305014b9836601a
Hossein <hossein.dehghanipour1998@gmail.com>
2020-08-31 00:31:45 +0430

    Merged my Own Homeworks with Gradery Files

---
+++ 3 - Sample Codes/IO/sample 3/code.c
@@ -0,0 +1,93 @@
+
+#include <mega32.h>
+
+// Alphanumeric LCD functions
+#include <alcd.h>
+
+// we need to use sprintf function
+#include <stdio.h>
+
+// delay functions
+#include <delay.h>
+
+// Declare your global variables here
+
+void main(void)
+{
+// Declare your local variables here
+
+
+//define a string and an integer
+
+// if you dont initialize str you get errors , TRY
+char * str = "";
+
+int input = 0;
+
+
+// Input/Output Ports initialization
+// Port A initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=Out 
+DDRA=(0<<DDA7) | (0<<DDA6) | (0<<DDA5) | (0<<DDA4) | (0<<DDA3) | (0<<DDA2) | (0<<DDA1) | (1<<DDA0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=0 
+PORTA=(0<<PORTA7) | (0<<PORTA6) | (0<<PORTA5) | (0<<PORTA4) | (0<<PORTA3) | (0<<PORTA2) | (0<<PORTA1) | (0<<PORTA0);
+
+// Port B initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
+DDRB=(0<<DDB7) | (0<<DDB6) | (0<<DDB5) | (0<<DDB4) | (0<<DDB3) | (0<<DDB2) | (0<<DDB1) | (0<<DDB0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
+PORTB=(0<<PORTB7) | (0<<PORTB6) | (0<<PORTB5) | (0<<PORTB4) | (0<<PORTB3) | (0<<PORTB2) | (0<<PORTB1) | (0<<PORTB0);
+
+// Port C initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
+DDRC=(0<<DDC7) | (0<<DDC6) | (0<<DDC5) | (0<<DDC4) | (0<<DDC3) | (0<<DDC2) | (0<<DDC1) | (0<<DDC0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
+PORTC=(0<<PORTC7) | (0<<PORTC6) | (0<<PORTC5) | (0<<PORTC4) | (0<<PORTC3) | (0<<PORTC2) | (0<<PORTC1) | (0<<PORTC0);
+
+// Port D initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
+DDRD=(0<<DDD7) | (0<<DDD6) | (0<<DDD5) | (0<<DDD4) | (0<<DDD3) | (0<<DDD2) | (0<<DDD1) | (0<<DDD0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
+PORTD=(0<<PORTD7) | (0<<PORTD6) | (0<<PORTD5) | (0<<PORTD4) | (0<<PORTD3) | (0<<PORTD2) | (0<<PORTD1) | (0<<PORTD0);
+
+// Alphanumeric LCD initialization
+// Connections are specified in the
+// Project|Configure|C Compiler|Libraries|Alphanumeric LCD menu:
+// RS - PORTC Bit 0
+// RD - PORTC Bit 1
+// EN - PORTC Bit 2
+// D4 - PORTC Bit 4
+// D5 - PORTC Bit 5
+// D6 - PORTC Bit 6
+// D7 - PORTC Bit 7
+// Characters/line: 16
+lcd_init(16);
+
+while (1)
+      {
+      // Place your code here
+      
+      // We use PIN 6 , 7 from PORT A as inputs:
+      if(PINA.6 == 1 && PINA.7 == 1)
+        input = 3;
+      else if(PINA.6 == 0 && PINA.7 == 1)
+        input = 2;
+      else if(PINA.6 == 1 && PINA.7 == 0)
+        input = 1;
+      else 
+        input = 0;
+      
+      //check input   
+      if(input != 0) PORTA.0 = 1;
+      else PORTA.0 = 0;
+      
+      // writes string in second parameter(completed by third parameter) into STR  
+      sprintf(str , "input : %d" , input);
+      lcd_puts(str);
+        
+      // wait and clear LCD 
+      delay_ms(50);
+      lcd_clear();
+      
+      }
+}
